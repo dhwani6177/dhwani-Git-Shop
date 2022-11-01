3d3a164c102d940739d0599de19263d5cb74601c
dhara <fakemefakeme1234567@gmail.com>
2022-11-01 23:21:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 <<<<<<< HEAD
 print('Hello world :D')
@@ -7,3 +8,6 @@ print('Hello world :D')
 =======
 print('Hello world :D')
 >>>>>>> development
+=======
+print("Hello dhwani :D")
+>>>>>>> Stashed changes
